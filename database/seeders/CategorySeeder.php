9341fc141f26ab912843b65552db6a315c83e6c6
Elenenemstsveridze <elenenemstsveridzee@gmail.com>
2025-10-12 14:25:18 +0400

    Homework_3 commit

---
+++ database/seeders/CategorySeeder.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace Database\Seeders;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+
+class CategorySeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        DB::table('categories')->insert([
+            ['categoryName' => 'Technology'],
+            ['categoryName' => 'Sports'],
+            ['categoryName' => 'Music'],
+            ['categoryName' => 'Travel']
+        ]);
+    }
+}
